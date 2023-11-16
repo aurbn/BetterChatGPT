c13fa1c02a4eaeb2c798b2d1f9023d9978d8833f
Anatoly Urban <anatoly.urban@gmail.com>
2023-11-16 13:28:27 +0300

    Save.

--- src/types/chat.ts
+++ src/types/chat.ts
@@ -55,7 +55,9 @@ export type ModelOptions = 'gpt-4'
   | 'gpt-3.5-turbo-16k'
   | 'gpt-3.5-turbo-0613'
   | 'gpt-4-0314'
-  | 'gpt-4-0613';
+  | 'gpt-4-0613'
+  |'gpt-4-1106-preview'
+  |'gpt-3.5-turbo-1106';
 // | 'gpt-3.5-turbo-0301';
 // | 'gpt-4-0314'
 // | 'gpt-4-32k-0314'
